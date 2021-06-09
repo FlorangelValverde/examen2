7e1a2ebafd562445188c3a6bd1a440ceae04f825
FlorangelValverde <florangelvalverde@upeu.edu.pe>
2021-06-08 22:23:36 -0500

    conexion

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'root',
-    database: 'examen',
-    port: 5432
-})
+    host: 'ec2-54-166-242-77.compute-1.amazonaws.com',
+    user: 'kpjvdowxmkwyag',
+    password: '3f4fd6fe81496c0e6514b96042f9a0a04e5ee394e119fc0a7af9646d9cdadba7',
+    database: 'd659hsrfv2b1j2',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+});
